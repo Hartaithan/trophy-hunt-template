71cc87f501b2f815826156b0a6f3387d95bbaf47
Hartaithan <hartaithan@gmail.com>
2024-02-16 10:53:17 +0900

    install mantine

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ColorSchemeScript, MantineProvider } from "@mantine/core";
 import "./globals.css";
+import "@mantine/core/styles.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <ColorSchemeScript defaultColorScheme="dark" />
+      </head>
+      <body className={inter.className}>
+        <MantineProvider defaultColorScheme="dark">{children}</MantineProvider>
+      </body>
     </html>
   );
 }
