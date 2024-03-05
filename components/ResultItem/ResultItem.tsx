bb310d030b5399c5c28dcdbd658447b62fe3a23e
Hartaithan <hartaithan@gmail.com>
2024-03-05 09:47:31 +0900

    add adding game status notifications

--- components/ResultItem/ResultItem.tsx
+++ components/ResultItem/ResultItem.tsx
@@ -2,11 +2,13 @@
 
 import type { SearchResult } from "@/models/SearchModel";
 import { ActionIcon, Flex, Text } from "@mantine/core";
-import type { FC } from "react";
+import { useCallback, type FC } from "react";
 import classes from "./ResultItem.module.css";
 import IconPlus from "@/icons/IconPlus";
 import { addGame } from "@/actions/add-game";
 import ResultImage from "../ResultImage/ResultImage";
+import { notifications } from "@mantine/notifications";
+import { IconAlertOctagon, IconCheck } from "@tabler/icons-react";
 
 interface Props {
   item: SearchResult;
@@ -14,6 +16,42 @@ interface Props {
 
 const ResultItem: FC<Props> = (props) => {
   const { item } = props;
+
+  const handleAdd = useCallback((url: string) => {
+    notifications.show({
+      id: "add",
+      loading: true,
+      title: "Adding a game...",
+      message:
+        "The game is being added, it shouldn't take long, don't reload the page.",
+      autoClose: false,
+      withCloseButton: false,
+    });
+    addGame(url)
+      .then((res) => {
+        notifications.update({
+          id: "add",
+          loading: false,
+          title: "Success!",
+          message: res.message,
+          icon: <IconCheck size="1rem" />,
+          autoClose: 3000,
+        });
+      })
+      .catch(() => {
+        notifications.update({
+          id: "add",
+          loading: false,
+          color: "red",
+          title: "Something went wrong!",
+          message:
+            "For some reason the game didn't get added, please try again.",
+          icon: <IconAlertOctagon size="1rem" />,
+          withCloseButton: true,
+        });
+      });
+  }, []);
+
   return (
     <Flex className={classes.container}>
       <ResultImage item={item} />
@@ -36,7 +74,9 @@ const ResultItem: FC<Props> = (props) => {
           <Text className={classes.platform}>{item.platforms?.join(", ")}</Text>
         )}
       </Flex>
-      <ActionIcon className={classes.button} onClick={() => addGame(item.url)}>
+      <ActionIcon
+        className={classes.button}
+        onClick={() => handleAdd(item.url)}>
         <IconPlus width={18} height={18} />
         <Text>Add</Text>
       </ActionIcon>
