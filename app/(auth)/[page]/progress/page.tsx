394b86b9bba2bd0b08ccfb17abb05848ad4acb1b
Hartaithan <hartaithan@gmail.com>
2024-03-09 16:28:23 +0900

    add progress page loader and response message

--- app/(auth)/[page]/progress/page.tsx
+++ app/(auth)/[page]/progress/page.tsx
@@ -1,25 +1,41 @@
 import { updateProgress } from "@/actions/update-progress";
 import PageContainer from "@/components/PageContainer/PageContainer";
 import type { RequestPage } from "@/models/AppModel";
+import { Alert, Center, Loader, Skeleton } from "@mantine/core";
+import { Suspense, type FC } from "react";
 
-const ProgressPage: RequestPage = async (props) => {
+const Fallback: FC = () => (
+  <Center pos="relative" h={87} w="100%" maw={{ base: "100%", sm: 300 }}>
+    <Skeleton h="100%" w="100%" radius="md" />
+    <Loader pos="absolute" type="bars" />
+  </Center>
+);
+
+const Result: RequestPage = async (props) => {
   const {
     params: { page },
     searchParams: { session },
   } = props;
-
   const response = await updateProgress(page, session);
+  return (
+    <Alert
+      w="100%"
+      variant="light"
+      maw={{ base: "100%", sm: 300 }}
+      radius="md"
+      color={response?.status === "success" ? "accented" : "red"}
+      title={response?.status === "success" ? "Success!" : "Oops..."}>
+      {response?.message ?? "Message not found"}
+    </Alert>
+  );
+};
 
+const ProgressPage: RequestPage = async (props) => {
   return (
     <PageContainer w="100%" justify="center" align="center">
-      ProgressPage
-      <pre>
-        {JSON.stringify(
-          { props: props, page: page, session: session, response: response },
-          null,
-          2,
-        )}
-      </pre>
+      <Suspense fallback={<Fallback />}>
+        <Result {...props} />
+      </Suspense>
     </PageContainer>
   );
 };
