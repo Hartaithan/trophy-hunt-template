54995db5dbccf3a41ed02b8c9542887ae76d9a10
Hartaithan <hartaithan@gmail.com>
2024-03-17 16:01:15 +0900

    add github auth actions

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,11 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 
-const clientId = process.env.NEXT_PUBLIC_GITHUB_ID as string;
-const clientSecret = process.env.NEXT_PUBLIC_GITHUB_SECRET as string;
+const clientId = process.env.GITHUB_ID as string;
+const clientSecret = process.env.GITHUB_SECRET as string;
 
-export const options = { providers: [GitHub({ clientId, clientSecret })] };
+const options = { providers: [GitHub({ clientId, clientSecret })] };
 
-export const handler = NextAuth(options);
+const handler = NextAuth(options);
 
 export { handler as GET, handler as POST };
--- components/DownloadSection/DownloadSection.tsx
+++ components/DownloadSection/DownloadSection.tsx
@@ -1,11 +1,29 @@
 "use client";
 
-import { useSession } from "next-auth/react";
-import type { FC } from "react";
+import { Text } from "@mantine/core";
+import { Button, Flex, Group } from "@mantine/core";
+import { useSession, signIn, signOut } from "next-auth/react";
+import { Fragment, type FC } from "react";
 
 const DownloadSection: FC = () => {
-  const { data: session } = useSession();
-  return <pre>session: {JSON.stringify(session, null, 2)}</pre>;
+  const session = useSession();
+  return (
+    <Flex direction="column" justify="center" align="center">
+      {session.status === "authenticated" ? (
+        <>
+          <Group>
+            <Button>Download</Button>
+            <Button onClick={() => signOut()}>Sign Out</Button>
+          </Group>
+          <Text fz="xs" component="pre" mt="lg">
+            session: {JSON.stringify(session, null, 2)}
+          </Text>
+        </>
+      ) : (
+        <Button onClick={() => signIn("github")}>Sign In</Button>
+      )}
+    </Flex>
+  );
 };
 
 export default DownloadSection;
