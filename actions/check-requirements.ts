b89784c9265d44bf2c0a85150db6ddd1b4b1aed9
Hartaithan <hartaithan@gmail.com>
2024-03-30 15:49:55 +0900

    add requirements flags in response

--- actions/check-requirements.ts
+++ actions/check-requirements.ts
@@ -3,12 +3,18 @@
 import type { ActionResponse } from "@/models/ActionModel";
 import { Octokit } from "@octokit/rest";
 
+interface Response {
+  follow: boolean;
+  star: boolean;
+  download: boolean;
+}
+
 const owner = process.env.NEXT_PUBLIC_OWNER ?? "";
 const repo = process.env.NEXT_PUBLIC_REPO ?? "";
 
 export const checkRequirements = async (
   token: string | undefined,
-): Promise<ActionResponse> => {
+): Promise<ActionResponse<Response>> => {
   if (!token) {
     return {
       status: "error",
@@ -29,22 +35,32 @@ export const checkRequirements = async (
   ];
 
   const [follow, star] = await Promise.allSettled(requirements);
-  if (follow.status === "rejected") {
-    return {
-      status: "error",
-      message: "Sorry, but you need to follow the profile",
-    };
+
+  const followFound = follow.status === "fulfilled";
+  const starFound = star.status === "fulfilled";
+
+  let message = "Unknown error";
+  if (!followFound) {
+    message = "Sorry, but you need to follow the profile";
   }
-  if (star.status === "rejected") {
-    return {
-      status: "error",
-      message: "Sorry, but you need to give the repository a star",
-    };
+  if (!starFound) {
+    message = "Sorry, but you need to give the repository a star";
+  }
+
+  const data = {
+    follow: followFound,
+    star: starFound,
+    download: followFound && starFound,
+  };
+
+  if (!followFound || !starFound) {
+    return { status: "error", message, data };
   }
 
   return {
     status: "success",
     message:
       "Thanks for the follow and star. You will be redirected to the template download link",
+    data,
   };
 };
