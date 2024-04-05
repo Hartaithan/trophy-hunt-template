77420c1d405a92d87dc2e71927e35622fea40d90
Hartaithan <hartaithan@gmail.com>
2024-04-05 10:18:40 +0900

    add tutorial notion section content

--- sections/TutorialNotionSection/TutorialNotionSection.tsx
+++ sections/TutorialNotionSection/TutorialNotionSection.tsx
@@ -1,11 +1,43 @@
 import type { FC } from "react";
-import { Text, Title } from "@mantine/core";
+import { Anchor, Text, Title } from "@mantine/core";
 
 const TutorialNotionSection: FC = () => {
   return (
     <div>
-      <Title order={2}>Set Up Your Notion Integration</Title>
-      <Text>TutorialNotionSection</Text>
+      <Title order={2} mb="sm">
+        Set Up Your Notion Integration
+      </Title>
+      <ul>
+        <Text component="li">
+          Go to&nbsp;
+          <Anchor
+            fw="bold"
+            c="accented.4"
+            href="https://www.notion.so/my-integrations"
+            target="_blank">
+            Notion
+          </Anchor>
+          &nbsp;and sign in to your account.
+        </Text>
+        <Text component="li">
+          Click the&nbsp;
+          <Text fw="bold" c="accented.4" span>
+            &quot;New Integration&quot;
+          </Text>
+          &nbsp;button, give your integration a name and click the&nbsp;
+          <Text fw="bold" c="accented.4" span>
+            &quot;Submit&quot;
+          </Text>
+          &nbsp;button.
+        </Text>
+        <Text component="li">
+          Copy your&nbsp;
+          <Text fw="bold" c="accented.4" span>
+            Internal Integration Secret.
+          </Text>
+          &nbsp;You&apos;ll need it later.
+        </Text>
+      </ul>
     </div>
   );
 };
