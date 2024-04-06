e5e55117518d9d60e9ddeeae11593df4314bc2ff
Hartaithan <hartaithan@gmail.com>
2024-04-06 10:29:10 +0900

    add tutorial app section content

--- sections/TutorialAppSection/TutorialAppSection.tsx
+++ sections/TutorialAppSection/TutorialAppSection.tsx
@@ -1,11 +1,67 @@
 import type { FC } from "react";
-import { Text, Title } from "@mantine/core";
+import { Anchor, Text, Title } from "@mantine/core";
 
 const TutorialAppSection: FC = () => {
   return (
     <div>
-      <Title order={2}>Start Adding Games!</Title>
-      <Text>TutorialAppSection</Text>
+      <Title order={2} mb="sm">
+        Start Adding Games!
+      </Title>
+      <ul>
+        <Text component="li">
+          Once you&apos;ve duplicated the template to your workspace, locate
+          the&nbsp;
+          <Text fw="bold" c="accented.4" span>
+            Database ID
+          </Text>
+        </Text>
+        <Text component="li">
+          You can find it in the template page link by selecting&nbsp;
+          <Text fw="bold" c="accented.4" span>
+            Share
+          </Text>
+          &nbsp;and&nbsp;
+          <Text fw="bold" c="accented.4" span>
+            Copy link
+          </Text>
+        </Text>
+        <Text component="li">
+          Either manually extract the ID from the link or use the handy tool
+          provided on the&nbsp;
+          <Anchor
+            fw="bold"
+            c="accented.4"
+            href="https://trophy-hunt-template.vercel.app/signIn"
+            target="_blank">
+            login page
+          </Anchor>
+        </Text>
+        <Text component="li">
+          On the&nbsp;
+          <Anchor
+            fw="bold"
+            c="accented.4"
+            href="https://trophy-hunt-template.vercel.app/signIn"
+            target="_blank">
+            login page
+          </Anchor>
+          , enter your&nbsp;
+          <Text fw="bold" c="accented.4" span>
+            Notion Secret
+          </Text>
+          &nbsp;and&nbsp;
+          <Text fw="bold" c="accented.4" span>
+            Database ID
+          </Text>
+        </Text>
+        <Text component="li">
+          <Text fw="bold" c="accented.4" span>
+            You&apos;re all set!
+          </Text>
+          &nbsp;Head over to the search page to start adding games to your
+          collection.
+        </Text>
+      </ul>
     </div>
   );
 };
