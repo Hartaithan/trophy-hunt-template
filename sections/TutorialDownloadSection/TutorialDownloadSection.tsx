f61910e4b4b6b477209c2a088439a2687f4eb25a
Hartaithan <hartaithan@gmail.com>
2024-04-06 10:20:41 +0900

    add tutorial download section content

--- sections/TutorialDownloadSection/TutorialDownloadSection.tsx
+++ sections/TutorialDownloadSection/TutorialDownloadSection.tsx
@@ -1,11 +1,50 @@
 import type { FC } from "react";
-import { Text, Title } from "@mantine/core";
+import { Anchor, Text, Title } from "@mantine/core";
 
 const TutorialDownloadSection: FC = () => {
   return (
     <div>
-      <Title order={2}>Download the Template</Title>
-      <Text>TutorialDownloadSection</Text>
+      <Title order={2} mb="sm">
+        Download the Template
+      </Title>
+      <ul>
+        <Text component="li">
+          Go to&nbsp;
+          <Anchor
+            fw="bold"
+            c="accented.4"
+            href="https://trophy-hunt-template.vercel.app/download"
+            target="_blank">
+            Download Page
+          </Anchor>
+          &nbsp;and sign in with your GitHub account
+        </Text>
+        <Text component="li">
+          Show your appreciation by following&nbsp;
+          <Anchor
+            fw="bold"
+            c="accented.4"
+            href="https://github.com/Hartaithan"
+            target="_blank">
+            profile
+          </Anchor>
+          &nbsp;and starring&nbsp;
+          <Anchor
+            fw="bold"
+            c="accented.4"
+            href="https://github.com/Hartaithan/trophy-hunt-template"
+            target="_blank">
+            the repository
+          </Anchor>
+        </Text>
+        <Text component="li">
+          Verify the requirements by clicking on&nbsp;
+          <Text fw="bold" c="accented.4" span>
+            Check requirements
+          </Text>
+          &nbsp;and then download the template
+        </Text>
+      </ul>
     </div>
   );
 };
