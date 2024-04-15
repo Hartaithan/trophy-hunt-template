569875f944b6d5055c9ef7171c8d93475f9a6f41
Hartaithan <hartaithan@gmail.com>
2024-04-15 12:07:28 +0900

    add language popover

--- forms/SearchForm/SearchForm.tsx
+++ forms/SearchForm/SearchForm.tsx
@@ -1,9 +1,22 @@
 "use client";
 
 import { defaultLanguage, languageOptions } from "@/constants/language";
-import { Flex, Loader, Select, TextInput } from "@mantine/core";
+import type { PopoverWidth } from "@mantine/core";
+import {
+  Anchor,
+  Flex,
+  Group,
+  InputLabel,
+  InputWrapper,
+  Loader,
+  Popover,
+  Select,
+  TextInput,
+} from "@mantine/core";
 import { memo } from "react";
 import type { FC, RefObject } from "react";
+import classes from "./SearchForm.module.css";
+import { Text } from "@mantine/core";
 
 interface Props {
   search: string;
@@ -12,17 +25,42 @@ interface Props {
   languageRef: RefObject<HTMLInputElement>;
 }
 
+const LanguagePopover: FC = memo(() => (
+  <Popover
+    classNames={{ dropdown: classes.dropdown }}
+    width={null as unknown as PopoverWidth}
+    position="top-end">
+    <Popover.Target>
+      <Anchor className={classes.link} component="button">
+        Not working?
+      </Anchor>
+    </Popover.Target>
+    <Popover.Dropdown>
+      <Text size="xs">
+        Multilingual trophy lists aren&apos;t available for all games. When a
+        game is added with English as its main language, it usually only has an
+        English trophy list.
+      </Text>
+    </Popover.Dropdown>
+  </Popover>
+));
+
 const SearchForm: FC<Props> = (props) => {
   const { search, isLoading, handleChange, languageRef } = props;
   return (
-    <Flex direction="column" w="100%" gap="md">
-      <Select
-        ref={languageRef}
-        label="Language"
-        placeholder="Select"
-        defaultValue={defaultLanguage}
-        data={languageOptions}
-      />
+    <Flex className={classes.container}>
+      <InputWrapper>
+        <Group className={classes.group}>
+          <InputLabel>Language</InputLabel>
+          <LanguagePopover />
+        </Group>
+        <Select
+          ref={languageRef}
+          placeholder="Select"
+          defaultValue={defaultLanguage}
+          data={languageOptions}
+        />
+      </InputWrapper>
       <TextInput
         label="Search"
         placeholder="Search..."
