9d5b364948e5a36af7762cfda57b719c4a2882d9
Hartaithan <hartaithan@gmail.com>
2024-04-19 16:37:24 +0900

    add backup fetch target

--- constants/variables.ts
+++ constants/variables.ts
@@ -1,6 +1,11 @@
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL ?? "";
 export const SERVICE_URL = process.env.NEXT_PUBLIC_SERVICE_URL ?? "";
-export const FETCH_URL = process.env.NEXT_PUBLIC_FETCH_URL ?? "";
-export const FETCH_API_KEY = process.env.NEXT_PUBLIC_FETCH_API_KEY ?? "";
-export const FETCH_HOST = process.env.NEXT_PUBLIC_FETCH_HOST ?? "";
+export const FETCH_TARGET = process.env.NEXT_PUBLIC_FETCH_TARGET ?? "";
+export const FETCH_DEFAULT_URL =
+  process.env.NEXT_PUBLIC_FETCH_DEFAULT_URL ?? "";
+export const FETCH_BACKUP_URL = process.env.NEXT_PUBLIC_FETCH_BACKUP_URL ?? "";
+export const FETCH_BACKUP_API_KEY =
+  process.env.NEXT_PUBLIC_FETCH_BACKUP_API_KEY ?? "";
+export const FETCH_BACKUP_HOST =
+  process.env.NEXT_PUBLIC_FETCH_BACKUP_HOST ?? "";
 export const TEMPLATE_URL = process.env.NEXT_PUBLIC_TEMPLATE_URL ?? "";
--- utils/page.ts
+++ utils/page.ts
@@ -1,7 +1,32 @@
-import { FETCH_API_KEY, FETCH_HOST, FETCH_URL } from "@/constants/variables";
+import {
+  FETCH_TARGET,
+  FETCH_DEFAULT_URL,
+  FETCH_BACKUP_URL,
+  FETCH_BACKUP_HOST,
+  FETCH_BACKUP_API_KEY,
+} from "@/constants/variables";
 import { examples } from "@/example";
 import type { Example } from "@/models/ExampleModel";
 import type { PageResponse } from "@/models/PageModel";
+
+export const getFetchTarget = (url: string) => {
+  const variable = FETCH_TARGET;
+  if (variable === "backup") {
+    return fetch(FETCH_BACKUP_URL, {
+      method: "POST",
+      body: JSON.stringify({ url }),
+      headers: {
+        "Content-Type": "application/json",
+        "X-RapidAPI-Key": FETCH_BACKUP_API_KEY,
+        "X-RapidAPI-Host": FETCH_BACKUP_HOST,
+      },
+    });
+  } else {
+    const fetchURL = new URL(FETCH_DEFAULT_URL);
+    fetchURL.searchParams.set("url", url);
+    return fetch(fetchURL);
+  }
+};
 
 export const fetchPage = async (
   url: string,
@@ -9,15 +34,7 @@ export const fetchPage = async (
 ): Promise<PageResponse | null> => {
   if (example) return examples[example];
   try {
-    const request = await fetch(FETCH_URL, {
-      method: "POST",
-      body: JSON.stringify({ url }),
-      headers: {
-        "Content-Type": "application/json",
-        "X-RapidAPI-Key": FETCH_API_KEY,
-        "X-RapidAPI-Host": FETCH_HOST,
-      },
-    });
+    const request = await getFetchTarget(url);
     const response = await request.json();
     if (!request.ok) throw Error(JSON.stringify(response));
     return response;
