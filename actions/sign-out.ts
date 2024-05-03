1bd82b64637dc7fb3674583ea18a6512e893f21b
Hartaithan <hartaithan@gmail.com>
2024-05-03 10:12:12 +0900

    fix sign ou action

--- actions/sign-out.ts
+++ actions/sign-out.ts
@@ -1,10 +1,14 @@
 "use server";
 
-import type { ActionResponse } from "@/models/ActionModel";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
-export const signOut = async (): Promise<ActionResponse> => {
-  cookies().delete("notion-token");
-  redirect("/add");
+export const signOut = async () => {
+  try {
+    cookies().delete("notion-token");
+  } catch (error) {
+    console.error("sign out error", error);
+  } finally {
+    redirect("/");
+  }
 };
