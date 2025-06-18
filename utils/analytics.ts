95f9cfdf8b9b4fadc36a1a9a2975cdd83e1a02f8
Hartaithan <hartaithan@gmail.com>
2025-06-18 16:10:25 +0900

    refactor analytics client to use singleton pattern

--- utils/analytics.ts
+++ utils/analytics.ts
@@ -1,3 +1,5 @@
+"use server";
+
 import { BASE_URL, POSTHOG_HOST, POSTHOG_KEY } from "@/constants/variables";
 import { cookies } from "next/headers";
 import type { PostHogOptions } from "posthog-node";
@@ -9,12 +11,21 @@ const options: PostHogOptions = {
   flushInterval: 0,
 };
 
+class PostHogClient {
+  private static instance: PostHog | null = null;
+  public static getInstance(): PostHog {
+    if (PostHogClient.instance) return PostHogClient.instance;
+    PostHogClient.instance = new PostHog(POSTHOG_KEY, options);
+    return PostHogClient.instance;
+  }
+}
+
 export const capture = async (
   event: string,
   properties?: Record<string | number, any>,
 ) => {
   const id = cookies().get("database-id");
-  const client = new PostHog(POSTHOG_KEY, options);
+  const client = PostHogClient.getInstance();
   const distinctId = id?.value || "anonymous";
   const allProperties = { ...properties, $current_url: BASE_URL + "/" };
   client.capture({ event, distinctId, properties: allProperties });
